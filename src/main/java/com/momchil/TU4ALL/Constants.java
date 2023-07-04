784d0df5936f66e5fee52a66bc09a1ec5524953b
Momchil <daiparimaina@gmail.com>
2023-07-04 21:00:11 +0300

    update github

--- src/main/java/com/momchil/TU4ALL/Constants.java
+++ src/main/java/com/momchil/TU4ALL/Constants.java
@@ -5,5 +5,4 @@ public final class Constants {
     public static final int FRIEND_STATUS_REQUESTED = 1;
     public static final int FRIEND_STATUS_ACCEPTED = 2;
     public static final int FRIEND_STATUS_BLOCKED = 3;
-
 }
